89f9d71a358887500a17df610318ab0b73681f3a
Hitesh Katkar <113781351+hiteshkatkar@users.noreply.github.com>
2026-04-18 10:37:21 -0700

    Update upload-url.js

--- API/upload-url.js
+++ API/upload-url.js
@@ -1,1 +1,12 @@
+import { put } from '@vercel/blob';
 
+export default async function handler(req, res) {
+  const { name } = JSON.parse(req.body);
+
+  const blob = await put(name, null, {
+    access: 'public',
+    addRandomSuffix: true,
+  });
+
+  res.json({ url: blob.url });
+}
